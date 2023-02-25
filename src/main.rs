b65cb407d2e1eb3eec6c2bceabc6ccbda52d865f
Chris <christopherc1331@gmail.com>
2023-02-25 15:56:27 -0600

    Added env var port value for deployment

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use std::net::SocketAddr;
 use axum::{routing::get, Router};
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    let port_result = env::var("PORT");
+    let port = match port_result  {
+        Ok(p) => p.parse::<u16>().unwrap(),
+        Err(_) => panic!("Port not provided"),
+    };
     let app = Router::new().route("/", get(handler));
-    let addr = SocketAddr::from(([0,0,0,0], 3000));
+    let addr = SocketAddr::from(([0,0,0,0], port));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
