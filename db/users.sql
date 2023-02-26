ab68db4071a24bdc979d7aaf7e7c105d54f93555
Chris <christopherc1331@gmail.com>
2023-02-26 01:55:15 -0600

    Stubbed create user api endpoint

---
+++ db/users.sql
@@ -0,0 +1,20 @@
+CREATE TABLE railway.user
+(
+    id         INT AUTO_INCREMENT,
+    first_name VARCHAR(300)                        NOT NULL,
+    last_name  VARCHAR(300)                        NOT NULL,
+    email      VARCHAR(300)                        NOT NULL,
+    password   VARCHAR(1000)                       NOT NULL,
+    country    VARCHAR(200)                        NOT NULL,
+    state      VARCHAR(200)                        NOT NULL,
+    city       VARCHAR(200)                        NOT NULL,
+    user_type  ENUM('artist', 'client')            NOT NULL,
+    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP NULL,
+    CONSTRAINT user_pk
+        PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX user_email_uindex
+    ON railway.user (email);
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::net::SocketAddr;
-use axum::{routing::get, Router};
 use std::env;
+use axum::{
+    routing::{get, post},
+    Router,
+    response::{Json},
+};
+use serde::Deserialize;
+use chrono::prelude::*;
+use serde_json::{Value, json};
 
 #[tokio::main]
 async fn main() {
@@ -9,8 +16,14 @@ async fn main() {
         Ok(p) => p.parse::<u16>().unwrap(),
         Err(_) => panic!("Port not provided"),
     };
-    let app = Router::new().route("/", get(handler));
+
+    let app = Router::new()
+        .route("/", get(hello_world_handler))
+        .route("/user", post(create_user_handler));
+
     let addr = SocketAddr::from(([0,0,0,0], port));
+    let addr_str = addr.to_string();
+    println!("=======Server running at address: {addr_str}=======");
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -18,7 +31,38 @@ async fn main() {
         .unwrap();
 }
 
-async fn handler() -> &'static str {
+
+#[derive(Deserialize)]
+enum UserType {
+    Artist,
+    Client
+}
+
+#[derive(Deserialize)]
+struct User {
+    id: i32,
+    first_name: String,
+    last_name: String,
+    email: String,
+    password: String,
+    country: String,
+    state: String,
+    city: String,
+    user_type: UserType,
+    created_ts: DateTime<Utc>,
+    updated_ts: DateTime<Utc>,
+}
+
+
+async fn hello_world_handler() -> &'static str {
 let str = "Hello, world!";
     return str;
+}
+
+async fn create_user_handler(Json(user): Json<User>) -> Json<Value> {
+    let response_json: Json<Value> = Json(json!({
+        "id": 1,
+    }));
+
+    return response_json;
 }
